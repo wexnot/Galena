45815a2108a580f3feb51623aa9da15979687942
Avraham Figueroa <53924803+wexnot@users.noreply.github.com>
2019-12-05 21:52:12 -0500

    added gui

--- galena.py
+++ galena.py
@@ -2,11 +2,11 @@ import imageio
 import numpy as np
 
 lena = imageio.imread('lena.png', pilmode='RGB')
-images = [] 
+images = []
 
 for x in range(0, 6):
-    noisy = lena + 3 * lena.std() * np.random.random(lena.shape)
+    noisy = lena + 10 * lena.std() * np.random.random(lena.shape)
     images.append(noisy)
 
 
-imageio.mimsave("myGalena.gif", images)
+imageio.mimsave("myGalena2.gif", images)
